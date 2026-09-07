eb9cfd62d61360348f6968f012d6b5f81ecff09e
malaramofficial <malaramofficial@gmail.com>
2026-09-07 16:13:17 +0530

    Add Firebase Storage for listing photos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation("com.google.firebase:firebase-storage")
     implementation(libs.play.services.auth)
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
